a8301fe5c54acfc1756c7017691fd9543b9b18e6
Theo van Dijk <thvdijk@gmail.com>
2017-06-06 17:50:41 +0200

    add mongodb

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+const MongoClient = require('mongodb').MongoClient;
+const mongoUrl = 'mongodb://user:password@localhost:17207/nodejs-backend';
+
+let db;
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
@@ -12,15 +17,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Data retrieved');
-    console.log('Data retrieved');
+    db.collection('user-data').findOne({_id: 'example-user'}, (err, result) => {
+        if (err) return console.log(err);
+
+        res.send(result);
+        console.log('Data retrieved');
+    });
 });
 
 app.post('/', (req, res) => {
-    res.json({result: req.body});
     console.log('Post: ' + req.body);
+    let obj = req.body;
+    let idToken = req.query.idToken;
+
+    // validate token against google oauth provider
+    // if token is valid set the email that is returned by google as _id
+
+    obj._id = 'example-user';
+
+    db.collection('user-data').updateOne(obj, (err, result) => {
+        if (err) return console.log(err);
+
+        console.log('saved to database');
+        res.json({result: req.body});
+    });
 });
 
-app.listen(3000, function () {
-    console.log('listening on 3000');
+MongoClient.connect(mongoUrl, (err, database) => {
+    if (err) return console.log('Are you sure you have a mongodb server running?\n' + err);
+    db = database;
+    app.listen(3000, function () {
+        console.log('listening on 3000');
+    });
 });
+
